c270315d13679b38a9a4ee2d4ef72037f7e46d56
VilayD <55212942+VilayD@users.noreply.github.com>
2020-02-12 15:58:50 -0700

    Update drill_python.py

--- pyDrill/drill_python.py
+++ pyDrill/drill_python.py
@@ -3,40 +3,36 @@
 # Python 3.8.0
 
 
+# Python 3.8.0
+
+
 import os
 import time
 import datetime
 
 
 
-target_file = "drill_python.py"
-start_folder = "C:\\pyDrill\\"
-for root, dirs, files in os.walk(start_folder):
-    if target_file in files:
-        print(root + '\\' + target_file)
+def file_info():
+    # Print file path
+    target_file = "drill_python.py"
+    start_folder = "C:\\pyDrill\\"
+    for root, dirs, files in os.walk(start_folder):
+        if target_file in files:
+            print(root + '\\' + target_file)
 
 
 
+    # Iterate through start folder to check for .txt files and print it
+    print("\nLast Modified time for text files in this directory!\n")
+    for file in os.listdir(start_folder):
+        if file.endswith('.txt'):
+            abPath = os.path.join(start_folder, file)   
+            print(file, time.ctime(os.path.getmtime(file)))
+        
+        
 
-print('\n\nHere is the list!\n')
-for x in os.listdir('\\pyDrill'):
-    print (x)
-
-
-
-
-fName = 'drill_python.py'
-fPath = 'C:\\pyDrill\\'
-abPath = os.path.join(fPath, fName)
-print('Path:\n')
-print(abPath)
-
-
-
-
-for file in os.listdir('C:\\pyDrill\\'):
-    if file.endswith('.txt'):
-        print(file, "last modified: %s" % time.ctime(os.path.getmtime(file)))
+if __name__ == "__main__":
+    file_info()
         
 
 
